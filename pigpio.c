ad89f458c80a4f1629d100c99d2342db41e490fc
joan2937 <joan@abyz.me.uk>
2019-08-16 16:58:45 +0100

    V71+: #286 arm64 socket i/f response

--- pigpio.c
+++ pigpio.c
@@ -25,7 +25,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <http://unlicense.org/>
 */
 
-/* pigpio version 71 */
+/* pigpio version 71+ */
 
 /* include ------------------------------------------------------- */
 
