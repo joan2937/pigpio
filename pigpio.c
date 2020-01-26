d5dee1d0a55c78d0cb867abc6928cc810dcad3b7
Guy McSwain <guy.mcswain@gmail.com>
2020-01-25 21:40:36 -0600

    Bump to v74

--- pigpio.c
+++ pigpio.c
@@ -25,7 +25,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <http://unlicense.org/>
 */
 
-/* pigpio version 7303 */
+/* pigpio version 74 */
 
 /* include ------------------------------------------------------- */
 
