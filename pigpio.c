3ffe6634bdac51634403eea8c801ad233b15979c
Guy McSwain <guy.mcswain@gmail.com>
2020-04-29 23:46:36 -0500

    Bump revision to 7600.

--- pigpio.c
+++ pigpio.c
@@ -25,7 +25,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <http://unlicense.org/>
 */
 
-/* pigpio version 75 */
+/* pigpio version 76 */
 
 /* include ------------------------------------------------------- */
 
