d4e20ead60f50b0b6a5e864b7d993ccfdd3b7e35
Guy McSwain <guy.mcswain@gmail.com>
2020-06-26 16:20:36 -0500

    Bump version and generate docs.

--- pigpio.c
+++ pigpio.c
@@ -25,7 +25,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <http://unlicense.org/>
 */
 
-/* pigpio version 76 */
+/* pigpio version 77 */
 
 /* include ------------------------------------------------------- */
 
