88c18b120d15ee0d397ed2a0c3b018ecb7a24a13
root <root@h3.pi.smurf.noris.de>
2023-12-05 19:13:54 +0100

    Teach "pigs" to use the Unix socket

--- pigs.c
+++ pigs.c
@@ -37,6 +37,7 @@ This version is for pigpio version 69+
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/un.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 
@@ -109,40 +110,62 @@ static int initOpts(int argc, char *argv[])
 
 static int openSocket(void)
 {
-   int sock, err;
-   struct addrinfo hints, *res, *rp;
-   const char *addrStr, *portStr;
+   int sock;
+   const char *sockStr;
 
-   portStr = getenv(PI_ENVPORT);
-
-   if (!portStr) portStr = PI_DEFAULT_SOCKET_PORT_STR;
-
-   addrStr = getenv(PI_ENVADDR);
-
-   if (!addrStr) addrStr = PI_DEFAULT_SOCKET_ADDR_STR;
-
-   memset (&hints, 0, sizeof (hints));
-
-   hints.ai_family   = PF_UNSPEC;
-   hints.ai_socktype = SOCK_STREAM;
-   hints.ai_flags   |= AI_CANONNAME;
-
-   err = getaddrinfo(addrStr, portStr, &hints, &res);
-
-   if (err) return SOCKET_OPEN_FAILED;
-
-   for (rp=res; rp!=NULL; rp=rp->ai_next)
+   sockStr = getenv(PI_ENVSOCK);
+   if (sockStr && *sockStr)
    {
-      sock = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-
-      if (sock == -1) continue;
-
-      if (connect(sock, rp->ai_addr, rp->ai_addrlen) != -1) break;
+      struct sockaddr_un addr;
+      addr.sun_family = AF_LOCAL;
+      strncpy (addr.sun_path, sockStr, sizeof (addr.sun_path));
+      addr.sun_path[sizeof (addr.sun_path) - 1] = 0;
+      sock = socket (AF_LOCAL, SOCK_STREAM, 0);
+      if (sock > -1)
+      {
+         if (connect (sock, (struct sockaddr *) &addr, sizeof (addr)) == -1)
+         {
+            close(sock);
+            sock = -1;
+         }
+      }
    }
+   else
+   {
+      int err;
+      struct addrinfo hints, *res, *rp;
+      const char *addrStr, *portStr;
 
-   freeaddrinfo(res);
+      portStr = getenv(PI_ENVPORT);
 
-   if (rp == NULL) return SOCKET_OPEN_FAILED;
+      if (!portStr) portStr = PI_DEFAULT_SOCKET_PORT_STR;
+
+      addrStr = getenv(PI_ENVADDR);
+
+      if (!addrStr) addrStr = PI_DEFAULT_SOCKET_ADDR_STR;
+
+      memset (&hints, 0, sizeof (hints));
+
+      hints.ai_family   = PF_UNSPEC;
+      hints.ai_socktype = SOCK_STREAM;
+      hints.ai_flags   |= AI_CANONNAME;
+
+      err = getaddrinfo(addrStr, portStr, &hints, &res);
+
+      if (err) return SOCKET_OPEN_FAILED;
+
+      for (rp=res; rp!=NULL; rp=rp->ai_next)
+      {
+         sock = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+
+         if (sock == -1) continue;
+
+         if (connect(sock, rp->ai_addr, rp->ai_addrlen) != -1) break;
+      }
+
+      freeaddrinfo(res);
+      if (rp == NULL) return SOCKET_OPEN_FAILED;
+   }
 
    return sock;
 }
